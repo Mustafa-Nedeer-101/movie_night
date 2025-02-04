a5f730271db96b164aa099a86a99a9fcbb2271e6
Mustafa Nedeer <mustafanedeer1@gmail.com>
2025-02-04 22:31:31 +0200

    Implemented the main functionality of bottom navigation

---
+++ lib/features/navigation.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:movie_night/core/constants/colors.dart';
+import 'package:movie_night/features/home/presentation/home_screen.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _selectedIndex = 0;
+
+  // List of widgets to display for each tab
+  static final List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    DecoratedBox(decoration: BoxDecoration(color: Colors.orange)),
+    DecoratedBox(decoration: BoxDecoration(color: Colors.yellow)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: 'Saved'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: CColors.backgroundColor,
+      ),
+    );
+  }
+}
